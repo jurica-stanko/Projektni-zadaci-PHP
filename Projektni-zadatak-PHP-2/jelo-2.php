1e083f2acf7090af34fd2f0438c6aae4fdb34218
Jurica Stanko <65706736+jurica-stanko@users.noreply.github.com>
2022-12-28 22:18:26 +0100

    Add files via upload

---
+++ Projektni-zadatak-PHP-2/jelo-2.php
@@ -0,0 +1,34 @@
+<?php
+    print '
+    <h1>Glavno jelo 1</h1>
+    <div class = "wrapper">
+        <figure>
+            <img src = "slike/jelo5.jpeg" class = "news-slika">
+            <figcaption>ovo je opis prve slike</figcaption>
+        </figure>
+        <figure>
+            <img src = "slike/jelo6.jpeg" class = "news-slika">
+            <figcaption>ovo je opis druge slike</figcaption>
+        </figure>
+    </div>
+    <hr>
+    <div class = "clanak">
+        <h2>Podnaslov clanka</h2>
+        <h3>podpodnaslov clanka</h3>
+        <div class = "tekst-clanka">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.</p>
+            <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.</p>
+            <p>arious versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p>Source: <a href = "https://www.lipsum.com" target = "blank"> Lorem Ipsum</a></p>
+            <a href = "news.html" target = "blank">Back to news</a>
+        </div>
+    </div>
+    '
+?>
---
+++ Projektni-zadatak-PHP-2/jelo-4.php
@@ -0,0 +1,29 @@
+<?php
+    print '
+    <h1>Desert</h1>
+    <div class = "wrapper">
+        <figure>
+            <img src = "slike/jelo1.png" class = "news-slika">
+            <figcaption>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s/figcaption>
+        </figure>
+        <figure>
+            <img src = "slike/jelo2.jpg" class = "news-slika">
+            <figcaption>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s</figcaption>
+        </figure>
+    </div>
+    <hr>
+    <div class = "clanak">
+        <h2>Podnaslov clanka</h2>
+        <h3>Podpodnaslov clanka</h3>
+        <div class = "tekst-clanka">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.</p>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.</p>
+            <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.</p>
+            <p>arious versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p>Source: <a href = "https://www.lipsum.com" target = "blank"> Lorem Ipsum</a></p>
+            <a href = "news.html" target = "_blank">Back to news</a>
+        </div>
+    '
+?>
